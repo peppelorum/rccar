788110b759f8742a9db0c90560d0106dde575391
Peppe Hämeenniemi <p@bergqvi.st>
2019-11-14 13:01:47 +0100

    Added fireworks

--- js/rc.js
+++ js/rc.js
@@ -8,7 +8,26 @@ var app = new function() {
 
     var self = this;
 
-
+    self.dialog = async function() {
+        const { value: formValues } = await Swal.fire({
+            title: 'Multiple inputs',
+            html:
+              '<input id="swal-input1" class="swal2-input">' +
+              '<input id="swal-input2" class="swal2-input">',
+            focusConfirm: false,
+            preConfirm: () => {
+              return [
+                document.getElementById('swal-input1').value,
+                document.getElementById('swal-input2').value
+              ]
+            }
+          })
+          
+          if (formValues) {
+            Swal.fire(JSON.stringify(formValues))
+          }
+        
+    }
 
 
     setTimeout(function() {
@@ -17,4 +36,5 @@ var app = new function() {
 
     }, 1000);
 };
+
 
